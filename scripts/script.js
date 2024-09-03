3b368484d1197d7fb884d37b9c60f69cc4330780
alexsoserdotskiy <soserdotskiy@gmail.com>
2024-09-03 09:35:53 +0300

    finish modal hover

--- scripts/script.js
+++ scripts/script.js
@@ -3,10 +3,10 @@ const modal = document.querySelector('.modal');
 const closeButton = document.querySelector('.close-modal-button');
 
 searchLink.addEventListener('click', (evt) => {
-    evt.preventDefault();
-    modal.classList.remove('modal-close');
+  evt.preventDefault();
+  modal.classList.remove('modal-close');
 })
 
 closeButton.addEventListener('click', () => {
-    modal.classList.add('modal-close');
+  modal.classList.add('modal-close');
 })
